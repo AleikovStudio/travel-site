266b09c239d42a28a13cab612fdbb20cf4567d59
Alex <golearnweb@gmail.com>
2020-09-27 10:19:13 +0300

    photo

--- node-experiment/test.js
+++ node-experiment/test.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var https = require('https');
 
 fs.writeFile(__dirname + "/index.html", "<h1>HTML is great!</h1>", function (error) {
     if (error) {
@@ -6,4 +7,9 @@ fs.writeFile(__dirname + "/index.html", "<h1>HTML is great!</h1>", function (err
     } else {
         return console.log("Congrats!");
     }
+});
+
+var myPhotoLocation = "https://raw.githubusercontent.com/LearnWebCode/welcome-to-git/master/images/dog.jpg";
+https.get(myPhotoLocation, function (response) {
+    response.pipe(fs.createWriteStream(__dirname + "/mydog.jpg"));
 });
